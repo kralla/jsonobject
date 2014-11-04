97d1105bfcd26ea918148bf47858d9f63b071520
Sandro Nardi <sandro@nardi.li>
2014-11-04 07:54:54 +0100

    fix encoding

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    long_description = readme_file.read(encoding="utf-8")
+with open('README.md', encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name='jsonobject',
